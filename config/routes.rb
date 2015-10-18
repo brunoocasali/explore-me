a56f7bc0e2bbbb66f94bd15f553f374ee8cc607e
Bruno <brunoocasali@gmail.com>
2015-10-18 05:47:40 -0200

    removed unused devise views, and fixed home

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :welcome, only: :index
+  end
+
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   root 'welcome#index'
-
-  get 'welcome/index'
 end
