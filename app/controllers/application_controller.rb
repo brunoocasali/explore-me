fc82b1a991f4cfa879c8055d3218c9aa18267a1b
Bruno <brunoocasali@gmail.com>
2015-10-20 01:18:39 -0200

    fixed problem with facebook login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,3 @@
-require 'application_responder'
-
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -7,7 +5,15 @@ class ApplicationController < ActionController::Base
 
   respond_to :js, :html
 
-  def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || stored_location_for(resource) || admin_welcome_path
+  def after_sign_in_path_for(resource_or_scope)
+    if resource.sign_in_count == 1
+      admin_root_path
+    else
+      root_path
+    end
+  end
+
+  def new_session_path(scope)
+    root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
     resources :places
   end
 
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' },
+                     skip: [:registrations]
 
   root 'welcome#index'
 end
